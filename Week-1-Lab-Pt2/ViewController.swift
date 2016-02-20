791da2b043df2bffbb53da23822380bd64a229fa
NTP277 <walter.somerville@capitalone.com>
2016-02-20 15:46:10 -0500

    Initial Commit

---
+++ Week-1-Lab-Pt2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week-1-Lab-Pt2
+//
+//  Created by Somerville, Walter on 2/20/16.
+//  Copyright © 2016 Somerville, Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
